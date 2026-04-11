5e1933224cd6f5dc0332ed60913cdeeee07c9ed1
Anushka Bhandari <bhandarianushka20@gmail.com>
2026-04-11 11:47:13 +0530

    Implemented bubble sort in descending order

---
+++ bubbleSort_desc.java
@@ -0,0 +1,20 @@
+public class bubbleSort_desc {
+    public static void bubble_desc(int arr[]){
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=0; j<arr.length-i-1; j++){
+                if(arr[j] < arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void main(String args[]){
+        int arr[] = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
+        bubble_desc(arr);
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
